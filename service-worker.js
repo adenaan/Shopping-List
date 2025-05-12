4f6367bb44021d1bf10015361da18bf7f398fe84
Adenaan <adenaan@gmail.com>
2025-05-12 18:26:12 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
 const CACHE_NAME = 'shopping-list-cache-v1';
 const OFFLINE_URLS = [
   '/Shopping-List/',
+  '/Shopping-List/index.html',
   'https://adenaan.github.io/Shopping-List/index.html',
   '/Shopping-List/icon-192.png',
   '/Shopping-List/manifest.json',
